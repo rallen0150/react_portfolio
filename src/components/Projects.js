1e22511d3d0d74a6d1c5dc979eb7c009559b5395
rallen0150 <rallen0150@gmail.com>
2021-01-31 11:31:40 -0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -35,7 +35,7 @@ class Projects extends Component {
 
                 {/* Project 2 */}
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{height: '176px', background: 'url(images/project_pics/videogame-library/game-title.png) center / cover', cursor: 'pointer' }} >Video Game Library/Search</CardTitle>
+                    <CardTitle style={{height: '176px', background: 'url(images/project_pics/videogame-library/game-title.png) center / cover' }} >Video Game Library/Search</CardTitle>
                     <CardText className="project_desc">
                         This video game library was my first true React project. I wanted to use an external API to connect from a React app, so I built a video game search using the <a href='https://rawg.io/' target="_blank" rel="noopener noreferrer">Rawg.io API</a>. You get to search a game you want and then you get the description, screenshots, where to find/buy the game online, and related games to what you searched. You are also able to navigate to the system's, genre's and publisher's page which is a description and top games for them.
                     </CardText>
